c19242d80a2ef60edcf30a9c0e694183d25b12b0
Tijn van der Krol <tijnvanderkrol@hotmail.nl>
2025-05-30 13:57:36 +0200

    got a little further today, managed to get sensible 3d projections, but some work remains.

--- functions/processing/video_processing.py
+++ functions/processing/video_processing.py
@@ -42,3 +42,88 @@ def preprocess_video(vid_path_indi, img_dir, fps=10, verbose = 1):
     for i, frame in enumerate(sampled_frames):
         img_path = os.path.join(img_dir_indi, f'{os.path.basename(vid_path_indi).split(".")[0]}_{i+1}.jpg')
         cv2.imwrite(img_path, frame)
+
+def analyze_vid_2d(vid_filepath, box_model, pose_model):
+    import cv2
+    import os
+    import numpy as np
+    from functions.boxmodel import get_box
+    from functions.preprocess import preprocess_hrnet
+    from functions.keypoints import transform_keypoints_to_original
+
+    cap = cv2.VideoCapture(vid_filepath)
+    if not cap.isOpened():
+        raise IOError(f"Cannot open video file: {vid_filepath}")
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    print(f'Total frames in video: {total_frames}')
+    data_batch = {'Keypoints': [], 'Confidence': []}
+    for i in range(total_frames):
+        ret, frame = cap.read()
+        # halving the frames
+        if i%2 != 0 or not ret:
+            continue
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (640, 480))
+        box = get_box(image, box_model)
+        input_tensor, transformation_matrix, _ = preprocess_hrnet(image, box)
+        heatmaps = pose_model(input_tensor)
+        data, _ = transform_keypoints_to_original(heatmaps, input_tensor, transformation_matrix)
+
+        data_batch['Keypoints'].append(np.array([kp[:2] for kp in data]))
+        data_batch['Confidence'].append(np.array([kp[2] for kp in data]))
+
+        if i % 50 == 0:
+            print(f'Processed frame {np.ceil(i/2)}/{total_frames/2}')
+    cap.release()
+    return data_batch
+
+def revise_kpts(keypoints_batch, confidence_batch, proximity=3):
+    """ Replaces low confidence keypoints with nearby keypoints based on confidence thresholds, prioritizeing higher confidence keypoints.
+    Args:
+        keypoints_batch (np.ndarray): Array of shape (frames, joints, coords) containing keypoints.
+        confidence_batch (np.ndarray): Array of shape (frames, joints) containing confidence scores for each keypoint.
+        proximity (int): Number of frames to look ahead and behind for replacing low confidence keypoints.
+    Returns:
+        np.ndarray: Updated keypoints array with low confidence keypoints replaced.
+    """
+    import numpy as np
+    new_kpts = []
+    indices = []
+    proximity = 3
+    ranges = np.array(range(-proximity, proximity + 1))
+    # remove proximity from ranges
+    ranges = ranges[ranges != 0]
+    # sort on proximity to 6
+    ranges_sortes = np.argsort(np.abs(ranges) - proximity)
+    order = ranges[ranges_sortes]
+    for idx, confs in enumerate(confidence_batch):
+        low_conf_trigger = False
+        if np.any(confs < 0.3):
+            low_conf_trigger = True
+            keypoints = keypoints_batch[idx]
+            low_conf_indices = np.where(confs < 0.3)[0]
+            # only higher than joint 10 is relevant
+            low_conf_indices = low_conf_indices[low_conf_indices > 9]
+            for low_conf_idx in low_conf_indices:
+                confs_nearby = confidence_batch[idx-5:idx+6, low_conf_idx]
+                if np.any(confs_nearby >= 0.5):
+                    for r in order:
+                        if confidence_batch[idx + r, low_conf_idx] >= 0.5:
+                            keypoints[low_conf_idx] = keypoints_batch[idx + r, low_conf_idx]
+                            break
+                elif np.any(confs_nearby >= 0.3):
+                    for r in order:
+                        if confidence_batch[idx + r, low_conf_idx] >= 0.3:
+                            keypoints[low_conf_idx] = keypoints_batch[idx + r, low_conf_idx]
+                            break
+                else:
+                    keypoints[low_conf_idx] = keypoints_batch[idx, low_conf_idx]
+            
+        else:
+            new_kpts.append(keypoints_batch[idx])
+
+        if low_conf_trigger:
+            new_kpts.append(keypoints)
+            indices.append(idx)
+    new_kpts = np.array(new_kpts)
+    return new_kpts, indices
--- functions/visualization.py
+++ functions/visualization.py
@@ -23,3 +23,103 @@ def show_keypoints_on_image(img, keypoints):
     cv2.imshow('Keypoints', img_org)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def show2Dpose(kps, img):
+    import cv2
+
+    colors = [(138, 201, 38),
+              (25, 130, 196),
+              (255, 202, 58)] 
+
+    connections = [[15, 13], [13, 11], [16, 14], [14, 12], [11, 12], [5, 11], [6, 12], [5, 6], [5, 7], [6, 8], [7, 9], [8, 10], [1, 2],
+ [0, 1], [0, 2], [1, 3], [2, 4], [3, 5], [4, 6]]
+
+    LR = [1,1,1,1, 2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3]
+
+    thickness = 3
+
+    for j,c in enumerate(connections):
+        start = map(int, kps[c[0]])
+        end = map(int, kps[c[1]])
+        start = list(start)
+        end = list(end)
+        cv2.line(img, (start[0], start[1]), (end[0], end[1]), colors[LR[j]-1], thickness)
+        cv2.circle(img, (start[0], start[1]), thickness=-1, color=colors[LR[j]-1], radius=3)
+        cv2.circle(img, (end[0], end[1]), thickness=-1, color=colors[LR[j]-1], radius=3)
+
+    return img
+
+
+def show3Dpose(vals, ax, fix_z):
+    import numpy as np
+    ax.view_init(elev=15., azim=70)
+
+    colors = [(138/255, 201/255, 38/255),
+            (255/255, 202/255, 58/255),
+            (25/255, 130/255, 196/255)]
+
+    I = np.array([15, 13, 16, 14, 11, 5, 6, 5, 5, 6, 7, 8, 1, 0, 0, 1, 2, 3, 4])
+    J = np.array([13, 11, 14, 12, 12, 11, 12, 6, 7, 8, 9, 10, 2, 1, 2, 3, 4, 5, 6])
+
+    LR = [1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3]
+
+    for i in np.arange( len(I) ):
+        x, y, z = [np.array( [vals[I[i], j], vals[J[i], j]] ) for j in range(3)]
+        ax.plot(x, y, z, lw=3, color = colors[LR[i]-1])
+
+    RADIUS = 0.72
+
+    xroot, yroot, zroot = vals[0,0], vals[0,1], vals[0,2]
+    ax.set_xlim3d([-RADIUS+xroot, RADIUS+xroot])
+    ax.set_ylim3d([-RADIUS+yroot, RADIUS+yroot])
+
+    if fix_z:
+        left_z = max(0.0, -RADIUS+zroot)
+        right_z = RADIUS+zroot
+        # ax.set_zlim3d([left_z, right_z])
+        ax.set_zlim3d([0, 1.5])
+    else:
+        ax.set_zlim3d([-RADIUS+zroot, RADIUS+zroot])
+
+    ax.set_aspect('equal') # works fine in matplotlib==2.2.2 or 3.7.1
+
+    white = (1.0, 1.0, 1.0, 0.0)
+    ax.xaxis.set_pane_color(white) 
+    ax.yaxis.set_pane_color(white)
+    ax.zaxis.set_pane_color(white)
+
+    ax.tick_params('x', labelbottom = False)
+    ax.tick_params('y', labelleft = False)
+    ax.tick_params('z', labelleft = False)
+
+def showimage(ax, img):
+    import matplotlib.pyplot as plt
+    ax.set_xticks([])
+    ax.set_yticks([]) 
+    plt.axis('off')
+    ax.imshow(img)
+
+
+
+def analyze_frames_2D(vid_filepath, new_kpts, indices):
+    import cv2
+    from functions.processing.visualization import show2Dpose
+    cap = cv2.VideoCapture(vid_filepath)
+    if not cap.isOpened():
+        raise IOError(f"Cannot open video file: {vid_filepath}")
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    for i in range(total_frames):
+        ret, frame = cap.read()
+        if i%2 != 0 or not ret:
+            continue
+        keypoints_relevant = new_kpts[i//2]
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (640, 480))
+        img = show2Dpose(keypoints_relevant, img)
+        if i in indices:
+            cv2.imshow(f'ALERT: CORRECT POSE DETECTED at frame {i//2}', img)
+        else:
+            cv2.imshow(f'Frame {i//2}', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
